a7e77f86ac5c4992114c51fb736bea749a64f314
qurashi-akeel <qurashiakeel786+gh@gmail.com>
2025-02-05 11:21:03 +0530

    Refactor polls app views to use generic views; update index, detail, and results views for improved structure and readability.

--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,30 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 from django.shortcuts import render, get_object_or_404
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_questions": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    # context_object_name = "latest_questions"
+    # default is object_list and we are using it as it is: question_list
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/detail.html", context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/results.html", context)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
