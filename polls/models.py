698e119c4ff320406cbaca86604848a2b94818a1
qurashi-akeel <qurashiakeel786+gh@gmail.com>
2025-02-10 18:50:12 +0530

    Update polls app to extend recent publication check to 7 days; add tests for question publication status and improve queryset filtering in views.

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,8 @@ class Question(Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - timedelta(days=1)
+        now = timezone.now()
+        return now - timedelta(days=7) <= self.pub_date <= now
 
 
 class Choice(Model):
